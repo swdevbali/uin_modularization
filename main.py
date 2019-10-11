5cd084923bd5a30ab9079a198ddd6928f0dabd12
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2019-10-11 16:35:34 +0700

    Kecepatan tanpa fungsi

---
+++ main.py
@@ -0,0 +1,12 @@
+nama = 'Eko Wibowo'
+program = 'Gerak Lurus'
+
+print(f'Program {program} oleh {nama}')
+
+jarak = 1000
+waktu = 5 * 60
+kecepatan = jarak / waktu
+
+print(f'Jarak = {jarak/1000}km ditempuh dalam waktu = {waktu/60}menit')
+print(f'Sehingga kecepatan = {kecepatan} m/s')
+
